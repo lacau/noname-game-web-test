5110fdf6bb910d063e379504e5fce0f1013b150d
Lacau <maz_llacau@uolinc.com>
2016-03-09 13:32:41 -0300

    Add ajax post to login endpoint.

--- js/login.js
+++ js/login.js
@@ -1,3 +1,22 @@
 jQuery(document).ready(function() {
 	
+	jQuery("#login-form").submit(function(event) {
+		var url = "/rest/login/credential";
+		var formData = jQuery("#login-form").serializeArray();
+
+		jQuery.ajax({
+		    url: url,
+		    type: 'post',
+		    data: formData,
+		    headers: {
+		        "Accept": 'application/json'
+		    },
+		    dataType: 'json',
+		    success: function (data) {
+		        console.info(data);
+		    }
+		});
+
+		event.preventDefault();
+	});
 });
