a3e77776d76d667ab165bbcbecbe335d477d714e
Lacau <maz_llacau@uolinc.com>
2016-03-14 12:13:37 -0300

    Add simple node server to test ajax requests.

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(res, res) {
+	res.sendFile(__dirname + '/view/index.html');
+});
+
+app.use('/js', express.static(__dirname + '/js'));
+app.use('/bower_components', express.static(__dirname + '/bower_components'));
+app.use('/css', express.static(__dirname + '/css'));
+
+// Listen
+app.listen(3000, function() {
+	console.log('Listening on port 3000...');
+});
