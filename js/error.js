3e4d6fd2aa79d3b5fd30940d462191fe5c3f446b
Lacau <maz_llacau@uolinc.com>
2016-03-15 17:36:51 -0300

    Refine error look and feel and handler.

--- js/error.js
+++ js/error.js
@@ -1,29 +1,43 @@
-var _block;
-
-function showErrorPopup() {
+function showErrorPopup(text) {
 	var body = jQuery('body');
 	blockScreen();
-	var errorDiv = createErrorDiv();
+	var errorDiv = createErrorDiv(text);
 	body.append(errorDiv);
 	errorDiv.center();
 }
 
 function blockScreen() {
-	_block = jQuery(document.createElement('div'));
+	var _block = jQuery(document.createElement('div'));
 	_block.addClass('block-screen');
 	var _width = ($(window).width() + $(window).scrollLeft()) + 'px';
 	var _height = ($(window).height() + $(window).scrollTop()) + 'px';
 	_block.css({width: _width, height: _height});
+	_block.attr('id', 'block-screen');
 	jQuery('body').append(_block);
 }
 
 function unblockScreen() {
-	jQuery('body').remove(_block);
+	jQuery('#error-container').remove();
+	jQuery('#block-screen').remove();
 }
 
-function createErrorDiv() {
+function createErrorDiv(description) {
 	var container = jQuery(document.createElement('div'));
 	container.addClass('error-container');
+	container.attr('id', 'error-container');
+	var titleDiv = jQuery(document.createElement('div'));
+	titleDiv.addClass('error-container-title');
+	var textDiv = jQuery(document.createElement('div'));
+	textDiv.addClass('error-container-text');
+	var button = jQuery(document.createElement('div'));
+	button.addClass('error-ok-button');
+	button.append('OK');
+	button.click(unblockScreen);
+	titleDiv.append('Error');
+	textDiv.append(description);
+	container.append(titleDiv);
+	container.append(textDiv);
+	container.append(button);
 	return container;
 }
 
--- js/login.js
+++ js/login.js
@@ -1,25 +1,28 @@
 jQuery(document).ready(function() {
 	
 	jQuery("#login-form").submit(function(event) {
-		// var url = "http://localhost:8080/noname-game/rest/login/credential";
-		// var _login = jQuery('input[name=login]').val();
-		// var _password = jQuery('input[name=password]').val();
-		// var formData = {login: _login, password: _password};
-		// jQuery.ajax({
-		//     url: url,
-		//     crossDomain: true,
-		//     type: 'post',
-		//     data: JSON.stringify(formData),
-		//     headers: {
-		//         "Accept": 'application/json',
-		//         "Content-Type": 'application/json'
-		//     },
-		//     dataType: 'json',
-		//     success: function (data) {
-		//         console.log(data);
-		//     }
-		// });
-	showErrorPopup();
+		var url = "http://localhost:8080/noname-game/rest/login/credential";
+		var _login = jQuery('input[name=login]').val();
+		var _password = jQuery('input[name=password]').val();
+		var formData = {login: _login, password: _password};
+		jQuery.ajax({
+		    url: url,
+		    crossDomain: true,
+		    type: 'post',
+		    data: JSON.stringify(formData),
+		    headers: {
+		        "Accept": 'application/json',
+		        "Content-Type": 'application/json'
+		    },
+		    dataType: 'json',
+		    success: function (data) {
+		        console.log(data);
+		    },
+		    error: function(xhr) {
+		    	console.log(xhr);
+		    	//showErrorPopup();
+		    }
+		});
 
 		event.preventDefault();
 	});
