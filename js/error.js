af3b0e1042431deeeebb147f23cadb0b9e72064d
Lacau <maz_llacau@uolinc.com>
2016-03-17 15:54:57 -0300

    Change center prototype location.

--- js/error.js
+++ js/error.js
@@ -65,12 +65,3 @@ function createErrorOkButton() {
 	div.click(unblockScreen);
 	return div;
 }
-
-jQuery.fn.center = function () {
-    this.css("position","absolute");
-    this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) + 
-                                                $(window).scrollTop()) + "px");
-    this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) + 
-                                                $(window).scrollLeft()) + "px");
-    return this;
-}
