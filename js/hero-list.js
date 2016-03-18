cd64722df416cb5e68a3dd38f3fec02df7b6e0ed
Lacau <maz_llacau@uolinc.com>
2016-03-18 16:40:44 -0300

    Create table header structure for heros list.

--- js/hero-list.js
+++ js/hero-list.js
@@ -1,3 +1,31 @@
 jQuery(document).ready(function() {
 	
+	function onload() {
+		var header = createTableHeader();
+		jQuery('#hero-table').append(header);
+	}
+
+	function createTableHeader() {
+		var columns = ['Name', 'Level', 'Action'];
+		var columnsLength = [40, 40, 20];
+		var headerDiv = jQuery(document.createElement('div'));
+		headerDiv.addClass('table-header');
+		headerDiv.append(createTableHeaderColumns(columns, columnsLength));
+
+		return headerDiv;
+	}
+
+	function createTableHeaderColumns(columns, columnsLength) {
+		var divs = [];
+		columns.forEach(function(el, index) {
+			var d = jQuery(document.createElement('div'));
+			d.addClass('table-header-column');
+			d.css({width: columnsLength[index] + '%'});
+			d.append(el);
+			divs.push(d);
+		});
+		return divs;
+	}
+
+	onload();
 });
