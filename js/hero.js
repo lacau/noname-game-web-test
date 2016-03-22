042d33ff458fefe0b99f6d670ade5497e64aed5d
Lacau <maz_llacau@uolinc.com>
2016-03-22 16:24:34 -0300

    Add hero status to hero page.

--- js/hero.js
+++ js/hero.js
@@ -1,5 +1,21 @@
 jQuery(document).ready(function() {
 
+	function onload() {
+		loadHeroStatus();
+	}
+
+	function loadHeroStatus() {
+		jQuery('#hero-level').append(selectedHero.level);
+		jQuery('#hero-str').append(selectedHero.str);
+		jQuery('#hero-hp').append(selectedHero.hp);
+		jQuery('#hero-dex').append(selectedHero.dex);
+		jQuery('#hero-stamina').append(selectedHero.stamina);
+		jQuery('#hero-patk').append(selectedHero.patk);
+		jQuery('#hero-exp').append(selectedHero.exp);
+		jQuery('#hero-pdef').append(selectedHero.pdef);
+	}
+
+	onload();
 });
 
 function beforeLoad() {
